00ef0a5eff2e0011083a1a107c600abeb8a1706d
Rafael Cappelletto <rafael_cappelletto@estudante.sesisenai.org.br>
2026-02-25 20:17:26 +0000

    update

--- atividade5.py
+++ atividade5.py
@@ -2,8 +2,9 @@ nome =input("Escreva o seu nome: ")
 
 idade =int(input("Escreva a sua idade: "))
 
-if idade <=120 and idade > 0:
-    dias_vida = idade * 365
-    print(f"Você ja viveu {dias_vida} dias")
-else:
-    print("Idade invalida")
+while idade >=120 or idade < 0:
+    idade =int(input("Escreva a sua idade: "))
+
+dias_vida = idade * 365
+print(f"{nome}, você ja viveu {dias_vida} dias")
+
