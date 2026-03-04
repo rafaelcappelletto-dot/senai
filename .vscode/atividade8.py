6658df874fbc690846a1a3e12703da14f19c2713
Rafael Cappelletto <rafael_cappelletto@estudante.sesisenai.org.br>
2026-03-04 19:16:10 +0000

    update

--- .vscode/atividade8.py
+++ .vscode/atividade8.py
@@ -1,14 +1,16 @@
 #Faça um algoritmo que receba o peso de uma pessoa, calcule e mostre:
 #a)  o novo peso se a pessoa engordar 15% sobre o peso digitado;
 #b)  o novo peso se a pessoa emagrecer 20% sobre o peso digitado.
-peso =0
+
+
+peso = float(input("Digite seu peso atual: "))
 
 while peso <=0 or peso > 600:
     peso = float(input("Digite seu peso atual: "))
 
 
-peso_engordar = peso + (0.15*peso)
-peso_emagrecer = peso - (0.20*peso)
+peso_engordar = peso + (peso * 0.15)
+peso_emagrecer = peso - (peso * 0.20)
 
 print("Se você engordar 15 porcento a mais em relação ao seu peso, seu novo peso será: ",peso_engordar)
-print("Se você emagrecer 20 porcento a mais, seu novo peso será: ",peso_emagrecer)
+print("Se você emagrecer 20 porcento a mais em relação ao seu peso, seu novo peso será: ",peso_emagrecer)
---
+++ .vscode/atividade9.py
@@ -0,0 +1,45 @@
+#Uma máquina mede a temperatura de um forno 10 vezes durante o dia.
+#Crie um algoritmo que:
+#Leia as 10 temperaturas
+#Informe:
+#A maior temperatura
+#A menor temperatura
+#A média das temperaturas
+#Quantas vezes a temperatura ultrapassou 100°C
+
+maior = float(0)
+menor = float(1000)
+acima_cem = int(0)
+soma = float(0)
+
+#verificando a temperatura do forno 10 vezes
+for cont in range(10):
+
+    cont += 1
+    temp = float(input(f"Informe a {cont} temperatura do forno: "))
+    
+
+    #Escolhendo a maior temperatura
+    if temp > maior:
+        maior = temp
+    
+    #escolhendo a menor temperatura
+    if temp < menor:
+        menor = temp
+
+    #somando as temperaturas
+    soma += temp
+
+    #Verifiacando quantas vezes a temperatua ultrapassou 100 graus
+    if temp > 100:
+        acima_cem += 1
+
+#Calculando a media
+media = soma / 10
+
+#Escrevendo os resultados
+print("A maior temperatura é: ",maior)
+print("A menor temperatura é: ",menor)
+print("A média das temperaturas é: ",media)
+print(f"A temperatura ultrapassou a marca de 100: {acima_cem} vezes")
+
