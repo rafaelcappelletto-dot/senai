aa3683235891aa7893440c54c69121f6ba4bc469
Rafael Cappelletto <rafael_cappelletto@estudante.sesisenai.org.br>
2026-03-04 17:56:36 +0000

    update

--- .vscode/atividade8.py
+++ .vscode/atividade8.py
@@ -1,12 +1,14 @@
 #Faça um algoritmo que receba o peso de uma pessoa, calcule e mostre:
 #a)  o novo peso se a pessoa engordar 15% sobre o peso digitado;
 #b)  o novo peso se a pessoa emagrecer 20% sobre o peso digitado.
+peso =0
 
-peso = float(input("Digite seu peso atual: "))
+while peso <=0 or peso > 600:
+    peso = float(input("Digite seu peso atual: "))
 
 
 peso_engordar = peso + (0.15*peso)
 peso_emagrecer = peso - (0.20*peso)
 
-print("Se você engordar 15 a mais em relação ao seu peso, seu novo peso será: ",peso_engordar)
+print("Se você engordar 15 porcento a mais em relação ao seu peso, seu novo peso será: ",peso_engordar)
 print("Se você emagrecer 20 porcento a mais, seu novo peso será: ",peso_emagrecer)
