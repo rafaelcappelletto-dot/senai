390f6fd3d3b94f37c8ea6babbb07d2c3e6103644
Rafael Cappelletto <rafael_cappelletto@estudante.sesisenai.org.br>
2026-03-11 19:21:46 +0000

    update

--- Atividade_Python/atividade10.py
+++ Atividade_Python/atividade10.py
@@ -8,27 +8,38 @@
 #Exibir alerta se alguma medição ultrapassar 200 PSI
 
 acima_15 = 0
-acima_20 =0
-soma =0
+acima_20 = 0
+acima_200 = 0
+sobrecarga = False
+alerta = False
+soma = 0
 
+for i in range(8):
 
-for cont in range(8):
-
-    corrente = int(input(f"Escreva a {cont+1} medição da corrente eletrica:"))
+    corrente = float(input(f"Escreva a {i+1} medição da corrente eletrica:"))
 
     if corrente > 15:
         acima_15 += 1
 
     if corrente > 20:
+        sobrecarga = True
         acima_20 += 1
 
     if corrente > 200:
+        alerta = True
+        acima_200 += 1
         print("ALERTA! A corrente ultrapassou 200")
 
     soma += corrente
 
-media =soma/cont
+#não colocar o [i] pq senao ele vai dividir por 9
+media = soma/8
 
-print(f"A corrente ficou acima de 15: {acima_15} vezes")
-print(f"Houve sobrecarga (acima de 20): {acima_20} vezes")
-print(f"A média das corrente é igual: {media}")
+print(f"A corrente ficou acima de 15A: {acima_15} vezes")
+if sobrecarga:
+    print(f"Houve sobrecarga (acima de 20A): {acima_20} vezes")
+if alerta:
+    print(f"Houve alerta (acima de 200A): {acima_200} vezes")
+print(f"A média das correntes é: {media} A")
+
+
