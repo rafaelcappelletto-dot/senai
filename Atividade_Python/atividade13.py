ddb6786fcc12383c927aa6ece2ccb556c5632922
Rafael Cappelletto <rafael_cappelletto@estudante.sesisenai.org.br>
2026-03-25 19:40:23 +0000

    update

--- Atividade_Python/atividade13.py
+++ Atividade_Python/atividade13.py
@@ -1,1 +1,4 @@
-input()
+num1 =int(input("Digite o primeiro número: "))
+num2 =int(input("Digite o segundo numero número: "))
+
+print(f"O resultado da tabuada do {num1} com o {num2} é igual :",num1 * num2)
